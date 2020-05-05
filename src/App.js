89f96e627ad2d4761336b1f9f7e20238e32a5943
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2020-05-05 12:06:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'typeface-roboto';
 import './App.css';
+var QRCode = require('qrcode.react');
+
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [location, setLocation] = useState("");
+  const [gender, setGender] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={wrapper}>
+      <h1>
+        Patientinmatning. 
+        </h1>
+      <form style={wrapper}>
+        <label style={ite2}>
+          Namn
+          <input
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+
+        <label style={ite2}>
+          Ålder:
+          <input
+            type="numbers"
+            value={age}
+            onChange={e => setAge(e.target.value)}
+          />
+        </label>
+        <label style={ite2}>
+          Plats:
+          <input
+            type="text"
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+
+        <label style={ite2}>
+          Kön:
+           <select
+            value={gender}
+            onChange={e => setGender(e.target.value)}>
+            <option value="M">Man</option>
+            <option value="F">Kvinna</option>
+            <option value="A">Icke Binär</option>
+
+          </select>
+        </label>
+      </form>
+      <QRCode value={name + "$" + age + "$" + location + "$" + gender} />
     </div>
   );
 }
+
+const wrapper = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  padding: '50px',
+  color: '#444',
+};
+
+
+const ite2 = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  padding: '5px',
+  color: '#444',
+};
 
 export default App;
